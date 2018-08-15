7ef4cad12024e9949c7d1a04f99396b237e2da07
Esteban <ehernandez@icci.cl>
2018-08-15 17:45:19 -0300

    Update main.cpp
    arreglo pifia del segundo if y printf

--- main.cpp
+++ main.cpp
@@ -28,14 +28,14 @@ int main(int argc, char** argv){
                 printf("procesador 1 recv numero %d desde el procesador numero 0",numero);
             }
         if(procesador==2){
-        int receptor = 3;
-        getline(ficheroEntrada, frase);
-        MPI_Send(&numero, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
-            }else if(procesador == 1){
-                int proc= 0;
+            int receptor = 3;
+            getline(ficheroEntrada, frase);
+            MPI_Send(&numero, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
+            }else if(procesador == 3){
+                int proc= 2;
                 MPI_Recv(&numero, 1, MPI_INT, proc, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
                 suma=suma+1;
-                printf("procesador 1 recv numero %d desde el procesador numero 0",numero);
+                printf("procesador 3 recv numero %d desde el procesador numero 2",numero);
             }
         }
     ficheroEntrada.close();
